962b73845843ed17dc7f091c00faaf62c9bf5ef9
danielle <n01160430@humbermail.ca>
2018-05-20 14:04:42 -0400

    Add files via upload

---
+++ randomNumberGenerator/randomNumberGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomNumberGenerator
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            Random rng = new Random();
+            for (int i= 0; i < 10; i++)
+                {
+                    Console.WriteLine("Your random number is:  ");
+                    Console.WriteLine(rng.Next(1, 101));
+                    Console.ReadLine();
+                
+                }
+
+           
+        }
+
+
+    }
+
+   
+
+}
